c3d72b7ae6f7b73509283ddbbd5bd7d3dcf5cebf
ragarunov <ironenag@gmail.com>
2017-03-08 16:06:06 -0500

    A5
    ASP.NET

---
+++ ASP.NET/Assignment5 - Copy/Assignment5/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+
+namespace Assignment5
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            // AutoMapper create map statements - using AutoMapper static API
+            // Mapper.Initialize(cfg => cfg.CreateMap< FROM , TO >());
+            // Add map creation statements here
+            Mapper.Initialize(cfg => {
+                // Add map creation statements here
+                cfg.CreateMap<Models.Album, Controllers.AlbumBase>();
+                cfg.CreateMap<Models.Artist, Controllers.ArtistBase>();
+                cfg.CreateMap<Models.MediaType, Controllers.MediaTypeBase>();
+
+                cfg.CreateMap<Models.Track, Controllers.TrackBase>();
+                cfg.CreateMap<Models.Track, Controllers.TrackWithDetail>();
+                cfg.CreateMap<Controllers.TrackAdd, Models.Track>();
+            });
+
+
+        }
+    }
+}
